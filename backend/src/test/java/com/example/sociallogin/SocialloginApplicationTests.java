21ed58dba65684acd9c12317c2dab1514b25896c
yoonms0617 <yoonminwater@gmail.com>
2022-06-19 13:04:30 +0900

    feat: 백엔드 프로젝트 초기 설정

---
+++ backend/src/test/java/com/example/sociallogin/SocialloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
