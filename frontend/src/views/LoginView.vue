0c5744e02d418ae92d2b75a30ea444aa2247c3f4
YoonMinsoo <yoonminwater@gmail.com>
2022-06-22 01:03:21 +0900

    view: 로그인 화면 추가 (#11)

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<template>
+  <login-form/>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  name: 'LoginView',
+  components: {
+    LoginForm
+  }
+}
+</script>
